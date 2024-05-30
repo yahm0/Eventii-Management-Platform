4c9b5aba202965ab38b9378b4cf5c4660d73dc1b
yahm0 <stnt190@outlook.com>
2024-05-29 20:05:40 -0400

    Added logic to the pages/Dashboard.jsx file.

---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import EventForm from '../components/Events/EventForm';
+
+const Dashboard = () => {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <EventForm />
+    </div>
+  );
+};
+
+export default Dashboard;
