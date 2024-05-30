9d23a32ca0edb81c1cb07d47cebb505276907af2
yahm0 <stnt190@outlook.com>
2024-05-29 20:10:58 -0400

    Added logic to the backend db.js

---
+++ backend/config/db.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    console.log('MongoDB connected');
+  } catch (error) {
+    console.error('MongoDB connection failed', error);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
