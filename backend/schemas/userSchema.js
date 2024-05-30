c9de21a9fbaa177e7475c01e8a1beb7dd2e2308f
yahm0 <stnt190@outlook.com>
2024-05-29 20:14:09 -0400

    Added logic to the userSchema for the types and inputs

---
+++ backend/schemas/userSchema.js
@@ -0,0 +1,33 @@
+const { gql } = require('apollo-server-express');
+
+const userSchema = gql`
+  type User {
+    id: ID!
+    name: String!
+    email: String!
+    events: [Event!]
+  }
+
+  input UserInput {
+    name: String!
+    email: String!
+    password: String!
+  }
+
+  type AuthPayload {
+    user: User!
+    token: String!
+  }
+
+  type Query {
+    users: [User!]
+    user(id: ID!): User
+  }
+
+  type Mutation {
+    signup(userInput: UserInput!): AuthPayload!
+    login(email: String!, password: String!): AuthPayload!
+  }
+`;
+
+module.exports = userSchema;
