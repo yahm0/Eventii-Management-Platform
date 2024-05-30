f9e46e062917f9cebe171864deead0b38c9c85ea
yahm0 <stnt190@outlook.com>
2024-05-29 20:13:50 -0400

    Added logic to the eventSchema for the types and inputs

---
+++ backend/schemas/eventSchema.js
@@ -0,0 +1,35 @@
+const { gql } = require('apollo-server-express');
+
+const eventSchema = gql`
+  type Event {
+    id: ID!
+    title: String!
+    description: String!
+    date: String!
+    location: String!
+    organizer: User!
+    attendees: [User!]
+    fee: Float
+  }
+
+  input EventInput {
+    title: String!
+    description: String!
+    date: String!
+    location: String!
+    fee: Float
+  }
+
+  type Query {
+    events: [Event!]
+    event(id: ID!): Event
+  }
+
+  type Mutation {
+    createEvent(eventInput: EventInput!): Event!
+    updateEvent(id: ID!, eventInput: EventInput!): Event!
+    deleteEvent(id: ID!): Event!
+  }
+`;
+
+module.exports = eventSchema;
