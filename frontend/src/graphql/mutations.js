b008696774ec124070e0666db02ace509433235c
yahm0 <stnt190@outlook.com>
2024-05-29 20:06:52 -0400

    Added logic to the mutations.js file for the login_user.

--- frontend/src/graphql/mutations.js
+++ frontend/src/graphql/mutations.js
@@ -12,3 +12,16 @@ export const SIGNUP_USER = gql`
     }
   }
 `;
+
+export const LOGIN_USER = gql`
+  mutation LoginUser($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      user {
+        id
+        name
+        email
+      }
+      token
+    }
+  }
+`;
