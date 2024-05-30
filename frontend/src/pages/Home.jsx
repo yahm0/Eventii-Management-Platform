0d434544c2293f1c46e45aefba010d5f3c5b95f7
yahm0 <stnt190@outlook.com>
2024-05-29 20:05:22 -0400

    Added logic to the pages/Home.jsx file.

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import EventList from '../components/Events/EventList';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to the Event Management Platform</h1>
+      <EventList />
+    </div>
+  );
+};
+
+export default Home;
