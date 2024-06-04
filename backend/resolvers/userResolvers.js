c0928c38410ca65bee9de551a70961c3d35d55c9
Landon Beard <lanbeard105@gmail.com>
2024-06-03 21:01:28 -0400

    Login is now working.

--- backend/resolvers/userResolvers.js
+++ backend/resolvers/userResolvers.js
@@ -3,10 +3,8 @@ const User = require('../models/User'); // Import the User model
 const userResolvers = {
   Mutation: {
     signup: async (_, { userInput }) => {
-      console.log('User input:', userInput); // Add this line to log user input
       const { name, email, password } = userInput;
       const user = new User({ name, email, password });
-      console.log('New user instance:', user); // Add this line to log user instance
       await user.save();
       const token = 'dummy-token'; // Replace with real token generation logic
       return { user, token };
