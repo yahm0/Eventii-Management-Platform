ff0ea633ce845b2fea396e0b14427a7f9719b643
yahm0 <stnt190@outlook.com>
2024-06-10 19:32:23 -0400

    changed the server.js.

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-const connectDB = async () => {
+const db = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
@@ -16,4 +16,4 @@ const connectDB = async () => {
   }
 };
 
-module.exports = connectDB;
+module.exports = db;
