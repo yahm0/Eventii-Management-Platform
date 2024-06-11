5c9aef06925cf5d42df8e06d0b760b567856c87a
yahm0 <stnt190@outlook.com>
2024-06-10 20:22:41 -0400

    changed the server.js.

--- backend/server.js
+++ backend/server.js
@@ -53,10 +53,10 @@ const startApolloServer = async () => {
   }));
 
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/dist')));
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
 
     app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+      res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
     });
   }
 
